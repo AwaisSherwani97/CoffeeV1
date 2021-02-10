6099738e3687566fab3cceaeebb220dd35917957
prisahotax <prisahotax@users.noreply.github.com>
2021-02-10 10:24:45 +0000

    Transaction Class

---
+++ Coffee/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coffee.Models
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public int ProductID { get; set; }
+        public DateTime TransactionDate{ get; set; }
+        public int Total { get; set; }
+    }
+}
